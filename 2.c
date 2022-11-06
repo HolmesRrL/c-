9c6fc81319f91ff1420591d35bbfdb725aa62179
HolmesRrL <117524076+HolmesRrL@users.noreply.github.com>
2022-11-06 21:14:14 +0800

    数据类型和变量常量
    % &

---
+++ 2.c
@@ -0,0 +1,36 @@
+/*#include<stdio.h>
+int main()
+{
+	int age=19;
+	double weight= 60.2;
+
+    age++;
+	weight=weight-5 ;
+	printf("%d\n",age);
+	printf("%lf\n",weight);
+    return 0;
+}*/
+
+
+
+
+/*#include<stdio.h>
+int main()
+{
+    int a=0;
+	int b=0;
+	int sum=0;
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("sum=%d\n",sum);
+	return 0;
+}*/
+
+
+
+
+
+
+
+
+  
